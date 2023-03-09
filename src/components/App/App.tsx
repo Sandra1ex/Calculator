c14fd55f940b057286f1ed7c3431508519fe5096
Alexandra <lubnina.alexandra@gmail.com>
2023-03-09 14:50:58 +0300

    feat: comp App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calc from '../Calc/Calc';
+import './App.css';
+
+function App() {
+  return (
+    <div className='side'>
+      <Calc/>
+    </div>
+  );
+}
+
+export default App;
