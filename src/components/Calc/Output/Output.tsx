7132533d23cc14459b3963ed70b8d16974d2a873
Alexandra <lubnina.alexandra@gmail.com>
2023-03-11 11:45:54 +0300

    feat: add output

---
+++ src/components/Calc/Output/Output.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+import "./Output.css";
+
+function Output() {
+  const { currentNumber, prevNumber, operand } = useSelector((store: RootState) => store.calcState);
+
+  return (
+    <div className="output">
+      <div className="previous-operand">{prevNumber} {operand}</div>
+      <div className="current-operand">{currentNumber}</div>
+    </div>
+  );
+}
+
+export default Output;
