e1c6f04b27ee5cf9e7c798258b62ca4f7909be8a
Alexandra <lubnina.alexandra@gmail.com>
2023-03-11 14:40:38 +0300

    fix: fixed style

--- src/components/Calc/Output/Output.tsx
+++ src/components/Calc/Output/Output.tsx
@@ -4,12 +4,17 @@ import { RootState } from "../../../store";
 import "./Output.css";
 
 function Output() {
-  const { currentNumber, prevNumber, operand } = useSelector((store: RootState) => store.calcState);
+  const { currentNumber, prevNumber, operand } = useSelector(
+    (store: RootState) => store.calcState
+  );
 
   return (
     <div className="output">
-      <div className="previous-operand">{prevNumber} {operand}</div>
+      <div className="previous-operand">
+        {prevNumber} {operand}
+      </div>
       <div className="current-operand">{currentNumber}</div>
+      {/* <input type="number" value={currentNumber} className="current-operand"/> */}
     </div>
   );
 }
