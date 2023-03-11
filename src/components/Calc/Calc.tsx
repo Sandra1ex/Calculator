21e552053696251f136adc80578260e9b64ac5b1
Alexandra <lubnina.alexandra@gmail.com>
2023-03-11 11:46:13 +0300

    feat: main comp

--- src/components/Calc/Calc.tsx
+++ src/components/Calc/Calc.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Number from "./Number/Number";
-import Operation from "./Operation/Operation";
 import Output from "./Output/Output";
-import Result from "./Result/Result";
 import "./Calc.css";
 
 function Calc() {
   return (
     <div className="calculator-grid">
       <Output />
-      <Operation />
       <Number />
-      <Result />
     </div>
   );
 }
